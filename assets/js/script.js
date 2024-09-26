e0bcb1fd1bc33ee77a40b88867e664e1e346df78
zahid ali khan <zahidalikhan207028@gmail.com>
2024-09-26 16:49:50 +0500

    changes

---
+++ assets/js/script.js
@@ -0,0 +1,114 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const menuToggleBtn = document.querySelector("[data-menu-toggle-btn]");
+
+menuToggleBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    menuToggleBtn.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * header sticky & back to top
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
+
+
+
+/**
+ * search box toggle
+ */
+
+const searchBtn = document.querySelector("[data-search-btn]");
+const searchContainer = document.querySelector("[data-search-container]");
+const searchSubmitBtn = document.querySelector("[data-search-submit-btn]");
+const searchCloseBtn = document.querySelector("[data-search-close-btn]");
+
+const searchBoxElems = [searchBtn, searchSubmitBtn, searchCloseBtn];
+
+for (let i = 0; i < searchBoxElems.length; i++) {
+  searchBoxElems[i].addEventListener("click", function () {
+    searchContainer.classList.toggle("active");
+    document.body.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * move cycle on scroll
+ */
+
+const deliveryBoy = document.querySelector("[data-delivery-boy]");
+
+let deliveryBoyMove = -80;
+let lastScrollPos = 0;
+
+window.addEventListener("scroll", function () {
+
+  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
+
+  if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
+    let activeScrollPos = window.scrollY;
+
+    if (lastScrollPos < activeScrollPos) {
+      deliveryBoyMove += 1;
+    } else {
+      deliveryBoyMove -= 1;
+    }
+
+    lastScrollPos = activeScrollPos;
+    deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
+  }
+
+});
+
+//for selected button to show
+  const filterButtons = document.querySelectorAll('.filter-btn');
+  const foodItems = document.querySelectorAll('.food-menu-list li');
+
+  filterButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      e.preventDefault(); 
+      const filterValue = button.getAttribute('data-filter');
+
+      filterButtons.forEach(btn => btn.classList.remove('active'));
+      button.classList.add('active');
+
+      foodItems.forEach(item => {
+        const itemCategory = item.getAttribute('data-category');
+        if (filterValue === 'all' || itemCategory === filterValue) {
+          item.style.display = 'block';
+        } else {
+          item.style.display = 'none'; 
+        }
+      });
+    });
+  });
+
